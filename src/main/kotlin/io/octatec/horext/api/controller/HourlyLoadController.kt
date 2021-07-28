6b85574f58d8eed2a839a3e9890168f67a58a525
Franz Bendezu <franzant20@gmail.com>
2021-07-28 18:03:33 -0500

    add hourly load

--- src/main/kotlin/io/octatec/horext/api/controller/HourlyLoadController.kt
+++ src/main/kotlin/io/octatec/horext/api/controller/HourlyLoadController.kt
@@ -1,9 +1,7 @@
 package io.octatec.horext.api.controller
 
-import io.octatec.horext.api.model.HourlyLoad
-import io.octatec.horext.api.model.OrganizationUnit
+import io.octatec.horext.api.domain.HourlyLoad
 import io.octatec.horext.api.service.HourlyLoadService
-import io.octatec.horext.api.service.OrganizationUnitService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
