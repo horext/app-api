ece8cb28437261e64ab9d0eb864dab6bf41e3e83
Franz Bendezu <franzant20@gmail.com>
2021-07-28 03:20:27 -0500

    add organization unit layers

--- src/main/kotlin/io/octatec/horext/api/domain/OrganizationUnit.kt
+++ src/main/kotlin/io/octatec/horext/api/domain/OrganizationUnit.kt
@@ -5,69 +5,35 @@ import org.ktorm.entity.Entity
 import org.ktorm.entity.sequenceOf
 import org.ktorm.schema.Table
 import org.ktorm.schema.int
+import org.ktorm.schema.long
 import org.ktorm.schema.varchar
 
-/**
- * The organization unit entity.
- */
 interface OrganizationUnit : Entity<OrganizationUnit> {
     companion object : Entity.Factory<OrganizationUnit>()
 
-    /**
-     * Organization Unit ID.
-     */
-    val id: Int
+    val id: Long
 
-
-    /**
-     * The organization's parent.
-     */
     var parentOrganizationUnit: OrganizationUnit?
 
-    /**
-     * Organization Unit name.
-     */
     var code: String
 
-    /**
-     * Organization Unit location.
-     */
     var name: String
 
-    /**
-     * Organization Unit type
-     */
     var type: OrganizationUnitType
 
 }
 
-/**
- * The organization unit table object.
- */
 object OrganizationUnits : Table<OrganizationUnit>("organization_unit") {
 
-    /**
-     * Organization Unit ID.
-     */
-    val id = int("id").primaryKey().bindTo { it.id }
+    val id = long("id").primaryKey().bindTo { it.id }
 
-    /**
-     * Organization Unit name.
-     */
+    val parentOrganizationId = long("parent_organization_id").bindTo { it.parentOrganizationUnit?.id }
+
     val code = varchar("code").bindTo { it.code }
 
-    /**
-     * Organization Unit location.
-     */
     val name = varchar("name").bindTo { it.name }
 
-    /**
-     * Organization Unit location.
-     */
     val typeId = int("organization_unit_type_id").references(OrganizationUnitTypes) { it.type }
 }
 
-/**
- * Return a default entity sequence of [OrganizationUnits].
- */
 val Database.organizationUnits get() = this.sequenceOf(OrganizationUnits)
--- src/main/kotlin/io/octatec/horext/api/service/OrganizationUnitService.kt
+++ src/main/kotlin/io/octatec/horext/api/service/OrganizationUnitService.kt
@@ -1,9 +1,9 @@
 package io.octatec.horext.api.service
 
-import io.octatec.horext.api.model.OrganizationUnit
+import io.octatec.horext.api.domain.OrganizationUnit
 
 interface OrganizationUnitService {
-    fun getAllSpeciality(): List<OrganizationUnit>
-    fun getAllFaculty(): List<io.octatec.horext.api.domain.OrganizationUnit>
+    fun getAllSpeciality():List<OrganizationUnit>
+    fun getAllFaculty(): List<OrganizationUnit>
     fun getAllSpecialityByFacultyId(id: Long): List<OrganizationUnit>
 }
