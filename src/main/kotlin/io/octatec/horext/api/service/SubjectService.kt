a4781cf20350f74d47822b2f406f0037b31b1c83
Franz Bendezu <franzant20@gmail.com>
2021-08-06 00:10:16 -0500

    add search subjects

--- src/main/kotlin/io/octatec/horext/api/service/SubjectService.kt
+++ src/main/kotlin/io/octatec/horext/api/service/SubjectService.kt
@@ -1,8 +1,23 @@
 package io.octatec.horext.api.service
 
-import io.octatec.horext.api.model.Subject
+import io.octatec.horext.api.domain.Subject
+import io.octatec.horext.api.dto.Page
+import org.ktorm.dsl.Query
 import org.springframework.util.MultiValueMap
 
+
 interface SubjectService {
-    fun getAllBySpecialityId(specialityId: Long, hourlyLoadId: Long): List<io.octatec.horext.api.domain.Subject>
+    fun getAllBySpecialityId(specialityId: Long, hourlyLoadId: Long): List<Subject>
+    fun getAllBySearchAndSpecialityIdAndHourlyLoad(
+        search: String,
+        specialityId: Long,
+        hourlyLoadId: Long
+    ): List<Subject>
+
+    fun getAllBySearchAndSpecialityIdAndHourlyLoad(
+        search: String,
+        specialityId: Long,
+        hourlyLoadId: Long,
+        offset: Int, limit: Int
+    ): Page<Subject>
 }
