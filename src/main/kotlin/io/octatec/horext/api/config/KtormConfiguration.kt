235192faa84d49a9bc18170f7f8386324a9ca157
Franz Bendezu <franzant20@gmail.com>
2024-02-28 10:07:09 -0500

    fix: remove unused imports and logger configuration in KtormConfiguration

--- src/main/kotlin/io/octatec/horext/api/config/KtormConfiguration.kt
+++ src/main/kotlin/io/octatec/horext/api/config/KtormConfiguration.kt
@@ -5,8 +5,6 @@ import org.ktorm.database.Database
 import org.ktorm.database.SqlDialect
 import org.ktorm.expression.SqlFormatter
 import org.ktorm.jackson.KtormModule
-import org.ktorm.logging.ConsoleLogger
-import org.ktorm.logging.LogLevel
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -18,14 +16,10 @@ class KtormConfiguration {
     @Autowired
     lateinit var dataSource: DataSource
 
-    /**
-     * Register the [Database] instance as a Spring bean.
-     */
     @Bean
     fun database(): Database {
         return Database.connectWithSpringSupport(
             dataSource = dataSource,
-            //logger = ConsoleLogger(threshold = LogLevel.DEBUG)
         )
     }
 
