4eed56920663a0d16ab9558de1bbe0d3cb836d11
Franz Bendezu <franzant20@gmail.com>
2024-03-01 09:06:30 -0500

    feat: add CustomExposedAutoConfiguration and remove KtormConfiguration for new dsl sql

---
+++ src/main/kotlin/io/octatec/horext/api/config/CustomExposedAutoConfiguration.kt
@@ -0,0 +1,31 @@
+package io.octatec.horext.api.config
+
+import org.jetbrains.exposed.spring.autoconfigure.ExposedAutoConfiguration
+import org.springframework.aot.hint.MemberCategory
+import org.springframework.aot.hint.RuntimeHints
+import org.springframework.aot.hint.RuntimeHintsRegistrar
+import org.springframework.aot.hint.TypeReference
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.ImportRuntimeHints;
+import java.util.*
+import kotlin.collections.LinkedHashMap
+
+@Configuration
+@ImportAutoConfiguration(
+    value = [ExposedAutoConfiguration::class]
+)
+@ImportRuntimeHints(CustomExposedAutoConfiguration.ExposedAutoConfigurationRuntimeHints::class)
+open class CustomExposedAutoConfiguration {
+
+    class ExposedAutoConfigurationRuntimeHints : RuntimeHintsRegistrar {
+        override fun registerHints(hints: RuntimeHints, classLoader: ClassLoader?) {
+            // see https://github.com/spring-projects/spring-boot/issues/34206
+            hints.reflection().registerType(Collections::class.java, *MemberCategory.entries.toTypedArray())
+            hints.reflection()
+                .registerType(TypeReference.of("java.util.Collections\$Entry"),
+                    *MemberCategory.entries.toTypedArray())
+        }
+    }
+}
