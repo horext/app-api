ba4db96f5ccf3cf67404e9e1de1a559fef2f5b49
Franz Bendezu <franzant20@gmail.com>
2024-03-01 11:54:22 -0500

    feat: ad unaccent function and ilike operator for exposed

--- src/main/kotlin/io/octatec/horext/api/util/PgFunctions.kt
+++ src/main/kotlin/io/octatec/horext/api/util/PgFunctions.kt
@@ -1,48 +1,74 @@
 package io.octatec.horext.api.util
 
-import org.ktorm.expression.ArgumentExpression
-import org.ktorm.expression.FunctionExpression
-import org.ktorm.schema.ColumnDeclaring
-import org.ktorm.schema.VarcharSqlType
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.Function
 
 
-
-
-public fun ColumnDeclaring<String>.unaccent(): FunctionExpression<String> {
-    // unaccent(str)
-    return FunctionExpression(
-        functionName = "unaccent",
-        arguments = listOf(this.asExpression()),
-        sqlType = VarcharSqlType
-    )
+class Unaccent<T : String?>(
+    private val expression: Expression<T>
+) : Function<T>(TextColumnType()) {
+    override fun toQueryBuilder(queryBuilder: QueryBuilder) {
+        queryBuilder { append("unaccent(", expression, ")") }
+    }
 }
 
-public fun ColumnDeclaring<String>.lower(): FunctionExpression<String> {
-    // lower(str)
-    return FunctionExpression(
-        functionName = "lower",
-        arguments = listOf(this.asExpression()),
-        sqlType = VarcharSqlType
-    )
+fun <T : String?> Expression<T>.unaccent(): Unaccent<T> = Unaccent(this)
+
+
+class UnaccentString(
+    val value: String
+) : Function<String>(TextColumnType()) {
+    override fun toQueryBuilder(queryBuilder: QueryBuilder) {
+        queryBuilder { append("unaccent(", stringParam(value), ")") }
+    }
+
+    override val columnType: IColumnType = VarCharColumnType()
 }
 
-fun concat(separator: String, vararg params: ColumnDeclaring<String>): FunctionExpression<String> {
-    return FunctionExpression(
-        functionName = "concat_ws",
-        arguments = listOf(ArgumentExpression(separator, VarcharSqlType)) + params.map { it.asExpression() },
-        sqlType = VarcharSqlType
-    )
+fun String.unaccent(): ExpressionWithColumnType<String> {
+    return UnaccentString(this)
 }
 
-public fun String.unaccent(): FunctionExpression<String> {
-    // unaccent(str)
-    return FunctionExpression(
-        functionName = "unaccent",
-        arguments = listOf(
-            ArgumentExpression(this, VarcharSqlType)
-        ),
-        sqlType = VarcharSqlType
-    )
+
+class IlikeEscapeOp(expr1: Expression<*>, expr2: Expression<*>, like: Boolean, private val escapeChar: Char?) :
+    ComparisonOp(expr1, expr2, if (like) "ILIKE" else "NOT ILIKE") {
+    override fun toQueryBuilder(queryBuilder: QueryBuilder) {
+        super.toQueryBuilder(queryBuilder)
+        if (escapeChar != null) {
+            with(queryBuilder) {
+                +" ESCAPE "
+                +stringParam(escapeChar.toString())
+            }
+        }
+    }
 }
+
+// Pattern Matching
+
+/** Checks if this expression matches the specified [pattern]. */
+infix fun <T : String?> Expression<T>.ilike(pattern: String) = ilike(LikePattern(pattern))
+
+/** Checks if this expression matches the specified [pattern]. */
+infix fun <T : String?> Expression<T>.ilike(pattern: LikePattern): IlikeEscapeOp =
+    IlikeEscapeOp(this, stringParam(pattern.pattern), true, pattern.escapeChar)
+
+/** Checks if this expression matches the specified [pattern]. */
+@JvmName("ilikeWithEntityID")
+infix fun Expression<EntityID<String>>.ilike(pattern: String) = ilike(LikePattern(pattern))
+
+/** Checks if this expression matches the specified [pattern]. */
+@JvmName("ilikeWithEntityID")
+infix fun Expression<EntityID<String>>.ilike(pattern: LikePattern): IlikeEscapeOp =
+    IlikeEscapeOp(this, stringParam(pattern.pattern), true, pattern.escapeChar)
+
+/** Checks if this expression matches the specified [expression]. */
+infix fun <T : String?> Expression<T>.ilike(expression: ExpressionWithColumnType<String>): IlikeEscapeOp =
+    IlikeEscapeOp(this, expression, true, null)
+
+/** Checks if this expression matches the specified [expression]. */
+@JvmName("ilikeWithEntityIDAndExpression")
+infix fun Expression<EntityID<String>>.ilike(expression: ExpressionWithColumnType<String>): IlikeEscapeOp =
+    IlikeEscapeOp(this, expression, true, null)
 
 
