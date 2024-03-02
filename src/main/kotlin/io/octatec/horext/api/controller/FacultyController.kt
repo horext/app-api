b3745eb5588323f9faece8c49a5125e1d241efdb
Franz Bendezu <franzant20@gmail.com>
2024-03-02 12:15:56 -0500

    feat: add getAllSpecialitiesByFacultyId endpoint to FacultyController

--- src/main/kotlin/io/octatec/horext/api/controller/FacultyController.kt
+++ src/main/kotlin/io/octatec/horext/api/controller/FacultyController.kt
@@ -1,8 +1,10 @@
 package io.octatec.horext.api.controller
 
 import io.octatec.horext.api.domain.OrganizationUnit
+import io.octatec.horext.api.exception.ResourceNotFoundException
 import io.octatec.horext.api.service.OrganizationUnitService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -14,5 +16,19 @@ class FacultyController(val organizationUnitService: OrganizationUnitService) {
     @GetMapping
     fun getAll(): List<OrganizationUnit> {
         return organizationUnitService.getAllFaculty()
+    }
+
+    @GetMapping(
+        "{facultyId}/specialities"
+    )
+    fun getAllSpecialitiesByFacultyId(
+        @PathVariable(name = "facultyId") facultyId: Long
+    ): List<OrganizationUnit> {
+        try {
+            return organizationUnitService.getAllSpecialityByFacultyId(facultyId)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            throw ResourceNotFoundException("OrganizationUnit")
+        }
     }
 }
