2694649600c479a43f7e55ec41f966f4ff462f3f
Franz Bendezu <franzant20@gmail.com>
2024-03-26 18:10:29 -0500

    feat: add migration for courses

---
+++ src/main/resources/db/migrations/V1_1_0__Courses.sql
@@ -0,0 +1,9 @@
+create table course
+(
+    id         varchar(255) not null
+        constraint course_pkey
+            primary key,
+    created_at timestamp,
+    updated_at timestamp,
+    name       varchar(255)
+);
