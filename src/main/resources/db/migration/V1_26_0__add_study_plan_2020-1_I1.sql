0baee378d7fff2953371cf6e31c427a63d81b064
Franz Bendezu <franzant20@gmail.com>
2024-03-27 01:23:22 -0500

    fix: remove specific schema for add study plan

--- src/main/resources/db/migration/V1_26_0__add_study_plan_2020-1_I1.sql
+++ src/main/resources/db/migration/V1_26_0__add_study_plan_2020-1_I1.sql
@@ -1,7 +1,7 @@
 insert into study_plan (id, code, from_date, name, to_date, organization_unit_id)
 values (15, '2020-1_I1', '2021-03-22 18:42:43.146000', null, null, 11);
 
-insert into public.subject (id, credits, cycle, max_cycle, min_cycle, note, required_credits, total_weekly_hours,
+insert into subject (id, credits, cycle, max_cycle, min_cycle, note, required_credits, total_weekly_hours,
                             weekly_laboratory_hours, weekly_practice_hours, weekly_practice_laboratory_hours,
                             weekly_theory_hours, course_id, evaluation_system_id, study_plan_id, subject_type_id, position)
 values (4138, 5, 1, null, null, null, null, 6, null, 2, null, 4, 'BQU01', 7, 15, 1, null),
