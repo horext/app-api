ee054081734b13227c29d7d023977792abfc5bb4
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-03-26 20:29:13 -0500

    fix: remove flyway-core

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,6 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	implementation("org.jetbrains.exposed:exposed-spring-boot-starter:0.48.0")
 	implementation("org.jetbrains.exposed:exposed-java-time:0.48.0")
-	implementation("org.flywaydb:flyway-core:10.0.0")
 }
 
 buildscript {
