2c6d8ce1e8d0f23213539e35b68500328ebce698
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2025-01-31 16:24:26 +0000

    style: agregar importación de java.util.Objects en ThreadServiceImpl.kt

--- src/main/kotlin/io/octatec/horext/api/service/ThreadServiceImpl.kt
+++ src/main/kotlin/io/octatec/horext/api/service/ThreadServiceImpl.kt
@@ -8,6 +8,7 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
+import java.util.Objects
 
 @Service
 class ThreadServiceImpl : ThreadService {
