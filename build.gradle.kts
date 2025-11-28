6eb9c6764c9fdd3a996d2f09bb6fda7f6592bc3b
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2025-11-28 13:27:01 -0500

    fix: upgrade Kotlin and Java versions in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 val ktlint by configurations.creating
 
 plugins {
-    kotlin("jvm") version "2.2.21"
-    kotlin("plugin.spring") version "2.2.21"
+    kotlin("jvm") version "2.3.0-RC"
+    kotlin("plugin.spring") version "2.3.0-RC"
     id("org.springframework.boot") version "4.0.0"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.graalvm.buildtools.native") version "0.11.3"
@@ -14,7 +14,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(24)
+        languageVersion = JavaLanguageVersion.of(25)
     }
 }
 
