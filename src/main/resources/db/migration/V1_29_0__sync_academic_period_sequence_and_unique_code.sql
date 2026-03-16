b20c3209194fb5ecfc2845a68cfaba63d3e14c8a
Franz Antony Bendezu Isidro <franzant20@gmail.com>
2026-03-16 06:43:59 +0000

    refactor(migration): remove obsolete SQL script for academic period synchronization

--- src/main/resources/db/migration/V1_29_0__sync_academic_period_sequence_and_unique_code.sql
+++
@@ -1,7 +0,0 @@
-SELECT setval(
-    pg_get_serial_sequence('academic_period', 'id'),
-    COALESCE((SELECT MAX(id) FROM academic_period), 1),
-    (SELECT MAX(id) IS NOT NULL FROM academic_period)
-);
-
-CREATE UNIQUE INDEX IF NOT EXISTS academic_period_code_uindex ON academic_period (code);
