222b86b19bcc1864514a48dd1b8e8f39cde92252
Franz Antony Bendezu Isidro <franzant20@gmail.com>
2026-05-20 23:17:05 +0000

    style: remove unnecessary blank line in ClassSessionService interface

--- src/main/kotlin/io/octatec/horext/api/service/ClassSessionService.kt
+++ src/main/kotlin/io/octatec/horext/api/service/ClassSessionService.kt
@@ -3,6 +3,5 @@ package io.octatec.horext.api.service
 import io.octatec.horext.api.domain.ClassSession
 
 interface ClassSessionService {
-
     fun findByScheduleIds(scheduleIds: List<Long>): List<ClassSession>
 }
