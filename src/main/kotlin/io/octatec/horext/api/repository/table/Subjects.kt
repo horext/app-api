6f71e83f1893d09af57fa2b0f7d4db8d515a0cf8
Franz Antony Bendezu Isidro <franzant20@gmail.com>
2026-08-18 01:54:48 +0000

    style: format code

--- src/main/kotlin/io/octatec/horext/api/repository/table/Subjects.kt
+++ src/main/kotlin/io/octatec/horext/api/repository/table/Subjects.kt
@@ -37,7 +37,7 @@ object Subjects : LongIdTable("subject") {
     val requiredCredits = integer("required_credits").nullable()
 
     val evaluationSystemId = long("evaluation_system_id").nullable()
-    
+
     val position = integer("position").nullable()
 
     fun createEntity(row: ResultRow): Subject =
