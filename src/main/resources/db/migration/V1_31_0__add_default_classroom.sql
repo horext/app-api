eb130017d954a9a910dd3e789b8751ce6369c666
Franz Antony Bendezu Isidro <franzant20@gmail.com>
2026-08-18 00:21:45 +0000

    fix: add default classroom and unique classroom code

--- src/main/resources/db/migration/V1_31_0__add_default_classroom.sql
+++ src/main/resources/db/migration/V1_31_0__add_default_classroom.sql
@@ -1,3 +1,6 @@
+ALTER TABLE classroom
+    ADD CONSTRAINT classroom_code_unique UNIQUE (code);
+
 INSERT INTO classroom (code, name)
 VALUES ('NO_CLASSROOM', 'Sin aula')
 ON CONFLICT (code) DO NOTHING;
