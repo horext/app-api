5c065b1f069232eeb58ecac61ddaf0a06ffe245b
Franz Antony Bendezu Isidro <franzant20@gmail.com>
2026-08-19 04:21:27 +0000

    feat: add standarize params for get subject by search

--- src/main/kotlin/io/octatec/horext/api/controller/SubjectController.kt
+++ src/main/kotlin/io/octatec/horext/api/controller/SubjectController.kt
@@ -21,10 +21,31 @@ class SubjectController(
     ): List<Subject> = subjectService.getAllBySpecialityId(specialityId, hourlyLoadId)
 
     @GetMapping(params = ["search", "speciality", "hourlyLoad"])
+    @Deprecated("speciality and hourlyLoad params are deprecated")
     fun getAllBySearch(
         @RequestParam(name = "search", required = true) search: String,
         @RequestParam(name = "speciality") specialityId: Long,
         @RequestParam(name = "hourlyLoad") hourlyLoadId: Long,
+        @RequestParam(name = "offset", defaultValue = "0") offset: Int,
+        @RequestParam(name = "limit", defaultValue = "10") limit: Int,
+    ): Page<Subject> {
+        Pagination.validatePageNumberAndSize(offset, limit)
+        val page =
+            subjectService.getPageBySearchAndSpecialityIdAndHourlyLoad(
+                search,
+                specialityId,
+                hourlyLoadId,
+                offset,
+                limit,
+            )
+        return page
+    }
+
+    @GetMapping(params = ["search", "specialityId", "hourlyLoadId"])
+    fun getPageBySpecialityId(
+        @RequestParam(name = "search", required = true) search: String,
+        @RequestParam(name = "specialityId") specialityId: Long,
+        @RequestParam(name = "hourlyLoadId") hourlyLoadId: Long,
         @RequestParam(name = "offset", defaultValue = "0") offset: Int,
         @RequestParam(name = "limit", defaultValue = "10") limit: Int,
     ): Page<Subject> {
