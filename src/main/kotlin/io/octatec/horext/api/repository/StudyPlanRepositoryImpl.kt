c546d8949f459499a622e9755c1dd7a2f3d2f1e5
Franz Antony Bendezu Isidro <franzant20@gmail.com>
2026-08-19 19:49:26 +0000

    refactor: add order by to study plans

--- src/main/kotlin/io/octatec/horext/api/repository/StudyPlanRepositoryImpl.kt
+++ src/main/kotlin/io/octatec/horext/api/repository/StudyPlanRepositoryImpl.kt
@@ -3,6 +3,7 @@ package io.octatec.horext.api.repository
 import io.octatec.horext.api.domain.StudyPlan
 import io.octatec.horext.api.repository.table.OrganizationUnits
 import io.octatec.horext.api.repository.table.StudyPlans
+import org.jetbrains.exposed.v1.core.SortOrder
 import org.jetbrains.exposed.v1.core.eq
 import org.jetbrains.exposed.v1.jdbc.select
 import org.jetbrains.exposed.v1.jdbc.selectAll
@@ -10,7 +11,12 @@ import org.springframework.stereotype.Repository
 
 @Repository
 class StudyPlanRepositoryImpl : StudyPlanRepository {
-    override fun getAllStudyPlan(): List<StudyPlan> = StudyPlans.selectAll().map { row -> StudyPlans.createEntity(row) }
+    override fun getAllStudyPlan(): List<StudyPlan> =
+        StudyPlans
+            .selectAll()
+            .orderBy(
+                StudyPlans.fromDate to SortOrder.DESC,
+            ).map { row -> StudyPlans.createEntity(row) }
 
     override fun getStudyPlanById(id: Long): StudyPlan? {
         val sp = StudyPlans
@@ -19,7 +25,9 @@ class StudyPlanRepositoryImpl : StudyPlanRepository {
             .leftJoin(ou)
             .select(sp.columns + ou.columns)
             .where { (sp.id eq id) }
-            .map { row -> sp.createEntity(row) }
+            .orderBy(
+                sp.fromDate to SortOrder.DESC,
+            ).map { row -> sp.createEntity(row) }
             .firstOrNull()
     }
 
@@ -30,6 +38,8 @@ class StudyPlanRepositoryImpl : StudyPlanRepository {
             .leftJoin(ou)
             .select(sp.columns + ou.columns)
             .where { (sp.organizationUnitId eq specialityId) }
-            .map { row -> sp.createEntity(row) }
+            .orderBy(
+                sp.fromDate to SortOrder.DESC,
+            ).map { row -> sp.createEntity(row) }
     }
 }
